6b0715a7075604f35b1162a3aec0c01a4dc11f2e
Khai221 <juliocnarciso@hotmail.com>
2024-09-22 19:26:35 -0400

    ajustando o código

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Você pode criar instâncias da classe Aluno aqui
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "João";
+        aluno1.curso = "Ciência da Computação";
+        aluno1.idade = 20;
 
-        // Classe
-        public class Aluno {
-            // Atributos da classe Aluno
-            String nome;
-            String curso;
-            int idade;
+        // Chamar métodos da classe Aluno
+        aluno1.assistirAula();
+        aluno1.fazerProva();
+        aluno1.calcularNota();
+    }
 
+    // Classe Aluno
+    public static class Aluno {
+        // Atributos da classe Aluno
+        String nome;
+        String curso;
+        int idade;
 
-            // Métodos
-            void assistirAula() {
-                // Definição do método
-            }
-            void fazerProva() {
-                // Definição do método
-            }
-            void calcularNota() {
-                // Definição do método
-            }
+        // Métodos
+        void assistirAula() {
+            // Definição do método
         }
 
+        void fazerProva() {
+            // Definição do método
+        }
+
+        void calcularNota() {
+            // Definição do método
+        }
     }
 }
